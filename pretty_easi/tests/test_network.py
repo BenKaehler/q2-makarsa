561ebbdd08c39a9c9498f81532a3aa2bc5e64653
Ben Kaehler <b.kaehler@adfa.edu.au>
2022-12-02 06:49:39 +1100

    linted

--- pretty_easi/tests/test_network.py
+++ pretty_easi/tests/test_network.py
@@ -1,11 +1,8 @@
 import unittest
-import networkx as nx
-from networkx import (read_graphml,Graph)
-import pandas as pd
-import qiime2
+
+from networkx import Graph, read_graphml
 from qiime2 import Artifact
 from qiime2.plugin.testing import TestPluginBase
-from pretty_easi._network import (Network, NetworkDirectoryFormat, NetworkFormat)
 
 
 class testnetwork(TestPluginBase):
@@ -14,26 +11,36 @@ class testnetwork(TestPluginBase):
     def setUp(self):
         super().setUp()
         self.network = self.get_data_path("network.graphml")
-        self.expected_network=read_graphml(self.network)
-        self.imported_network = Artifact.import_data("Network", self.expected_network)
+        self.expected_network = read_graphml(self.network)
+        self.imported_network = Artifact.import_data(
+            "Network", self.expected_network)
         self.qiime_network = self.imported_network.view(Graph)
-        
 
     def test_defaults(self):
-        my_list = [(a,b) for (a,b) in self.expected_network.degree() for (c,d) in self.qiime_network.degree() if ((a==c) and (b==d))]
+        my_list = [
+            (a, b)
+            for (a, b) in self.expected_network.degree()
+            for (c, d) in self.qiime_network.degree()
+            if ((a == c) and (b == d))
+        ]
         for i in self.qiime_network.degree():
             if i in my_list:
                 self.assertTrue(True)
             else:
                 self.assertTrue(False)
-        
-        
-        my_list_edges = [(a,b) for (a,b) in self.expected_network.edges() for (c,d) in self.qiime_network.edges() if ((a==c) and (b==d))or((a==d) and (b==c))]
+
+        my_list_edges = [
+            (a, b)
+            for (a, b) in self.expected_network.edges()
+            for (c, d) in self.qiime_network.edges()
+            if ((a == c) and (b == d)) or ((a == d) and (b == c))
+        ]
         for i in self.qiime_network.edges():
             if i in my_list_edges:
                 self.assertTrue(True)
             else:
                 self.assertTrue(False)
+
 
 if __name__ == "__main__":
     unittest.main()
--- pretty_easi/tests/test_spieceasi.py
+++ pretty_easi/tests/test_spieceasi.py
@@ -1,11 +1,10 @@
 import unittest
-import networkx as nx
-from networkx import read_graphml
+
 import pandas as pd
-import qiime2
+from networkx import read_graphml
+from pretty_easi._spieceasi import spiec_easi
 from qiime2 import Artifact
 from qiime2.plugin.testing import TestPluginBase
-from pretty_easi._spieceasi import spiec_easi
 
 
 class Testspieceasioutput(TestPluginBase):
@@ -14,28 +13,47 @@ class Testspieceasioutput(TestPluginBase):
     def setUp(self):
         super().setUp()
         self.empty_table = self.get_data_path("test2.biom")
-        self.empty_table = Artifact.import_data("FeatureTable[Frequency]", self.empty_table)
+        self.empty_table = Artifact.import_data(
+            "FeatureTable[Frequency]", self.empty_table
+        )
         self.df = self.empty_table.view(pd.DataFrame)
         self.network = self.get_data_path("out1.graphml")
-        self.generated_network = spiec_easi(self.df, method='mb',lambda_min_ratio=1e-2,nlambda=20,rep_num=50,ncores=16)  
-        self.expected_network=read_graphml(self.network)
-        #nx.draw(expected_network)
-        
-    def test_defaults(self):        
-        my_list = [(a,b) for (a,b) in self.expected_network.degree() for (c,d) in self.generated_network.degree() if ((a==c) and (b==d))]
+        self.generated_network = spiec_easi(
+            self.df,
+            method="mb",
+            lambda_min_ratio=1e-2,
+            nlambda=20,
+            rep_num=50,
+            ncores=16,
+        )
+        self.expected_network = read_graphml(self.network)
+        # nx.draw(expected_network)
+
+    def test_defaults(self):
+        my_list = [
+            (a, b)
+            for (a, b) in self.expected_network.degree()
+            for (c, d) in self.generated_network.degree()
+            if ((a == c) and (b == d))
+        ]
         for i in self.generated_network.degree():
             if i in my_list:
                 self.assertTrue(True)
             else:
                 self.assertTrue(False)
-        
-        
-        my_list_edges = [(a,b) for (a,b) in self.expected_network.edges() for (c,d) in self.generated_network.edges() if ((a==c) and (b==d))or((a==d) and (b==c))]
+
+        my_list_edges = [
+            (a, b)
+            for (a, b) in self.expected_network.edges()
+            for (c, d) in self.generated_network.edges()
+            if ((a == c) and (b == d)) or ((a == d) and (b == c))
+        ]
         for i in self.generated_network.edges():
             if i in my_list_edges:
                 self.assertTrue(True)
             else:
                 self.assertTrue(False)
+
 
 if __name__ == "__main__":
     unittest.main()
