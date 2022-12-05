aaac3777e3db75d4094b074e6d52d8e55f50169d
Zakir Hossine <zakirhossainmath@gmail.com>
2022-12-05 23:41:05 +1100

    statistic_of_network method created,initial stage

--- pretty_easi/_spieceasi.py
+++ pretty_easi/_spieceasi.py
@@ -1,10 +1,12 @@
 import tempfile
 import subprocess
 from pathlib import Path
-
 from networkx import read_graphml, Graph
-
 import pandas as pd
+import networkx as nx
+from operator import itemgetter
+import pickle
+import os
 
 
 def run_commands(cmds, verbose=True):  # EEE need to credit the authors of this
@@ -83,3 +85,33 @@ def spiec_easi(
 
         # EEE would be better to return this without reading it
         return read_graphml(str(network_file))
+    
+    
+
+
+def statistic_of_network(network):
+    
+    with tempfile.TemporaryDirectory() as temp_dir_name:
+        temp_dir = Path(temp_dir_name)
+        tmp_sorted_degree_centrality = os.path.join(temp_dir, 'sorted_degree_centrality')
+        tmp_sorted_betweenness_centrality = os.path.join(temp_dir, 'sorted_betweenness_centrality')
+        tmp_sorted_closeness_centrality = os.path.join(temp_dir, 'sorted_closeness_centrality')
+        tmp_sorted_eigenvector_centrality = os.path.join(temp_dir, 'sorted_eigenvector_centrality')
+        tmp_sorted_associativity = os.path.join(temp_dir, 'sorted_associativity')
+
+        sorted_degree_centrality=sorted(nx.degree_centrality(net).items(),key=itemgetter(1), reverse=True)
+        pickle.dump(sorted_degree_centrality,open('tmp_sorted_degree_centrality','wb'))
+        sorted_betweenness_centrality=sorted(nx.betweenness_centrality(net).items(),key=itemgetter(1),reverse=True)
+        pickle.dump(sorted_betweenness_centrality,open('tmp_sorted_betweenness_centrality','wb'))
+        sorted_closeness_centrality=sorted(nx.closeness_centrality(net).items(),key=itemgetter(1),reverse=True)
+        pickle.dump(sorted_closeness_centrality,open('tmp_sorted_closeness_centrality','wb'))
+        sorted_eigenvector_centrality=sorted(nx.eigenvector_centrality(net).items(),key=itemgetter(1),reverse=True)
+        pickle.dump(sorted_eigenvector_centrality,open('tmp_sorted_eigenvector_centrality','wb'))
+        sorted_associativity=sorted(nx.assortativity.average_neighbor_degree(net).items(),key=lambda e: e[1], reverse=True)
+        pickle.dump(sorted_associativity,open('tmp_sorted_associativity','wb'))
+        
+        #nx. assortativity.average_neighbor_degree(),calculates, for each node, an average of its neighbor’s degrees.
+        return(tmp_sorted_degree_centrality,tmp_sorted_betweenness_centrality,tmp_sorted_closeness_centrality,tmp_sorted_eigenvector_centrality,tmp_sorted_associativity)
+
+
+
