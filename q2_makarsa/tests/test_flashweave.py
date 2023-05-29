ec836011c1b9b960be3b84cfdc0b224a4ee6f426
IsaacTowers <i.towers@adfa.edu.au>
2023-05-29 12:41:30 +1000

    adding unit test for flashweave

---
+++ q2_makarsa/tests/test_flashweave.py
@@ -0,0 +1,17 @@
+from networkx import read_gml
+import networkx.algorithms.isomorphism as iso
+from qiime2.plugin.testing import TestPluginBase
+import biom
+
+from q2_makarsa._flashweave import flashweave
+
+
+class TestFlashweave(TestPluginBase):
+    package = "q2_makarsa.tests"
+
+    def test_defaults(self):
+        table = biom.load_table(self.get_data_path("table.biom"))
+        observed = flashweave(table)
+        expected = read_graphml(self.get_data_path("network-fw.gml"))
+        self.assertTrue(iso.is_isomorphic(observed, expected))
+
